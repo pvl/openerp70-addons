2262df2a12fe3200e8be067924a77f4d7cb03168
ddrapeau <david.drapeau@gmail.com>
2013-04-01 21:22:14 +0200

    add field for siret

--- crm_plus/crm_lead.py
+++ crm_plus/crm_lead.py
@@ -6,4 +6,5 @@ class crmLeadInherit(osv.osv):
     
     _columns = {
         'activity': fields.char("Activity", size=128),
+        'company_registration_number': fields.char("Company Registration Number", size=64),
     }
