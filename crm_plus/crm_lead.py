db957f1b2d59d091d46817801ca26cbb17e7b096
ddrapeau <david.drapeau@gmail.com>
2013-04-01 21:29:53 +0200

    [FIX] bug on employee quantity field

--- crm_plus/crm_lead.py
+++ crm_plus/crm_lead.py
@@ -8,5 +8,5 @@ class crmLeadInherit(osv.osv):
         'activity': fields.char("Activity", size=128),
         'company_registration_number': fields.char("Company Registration Number", size=64, help="SIREN/SIRET pour la France"),
         'company_professional_code': fields.char("Company Professional Code", size=64, help="Code NAF pour la France"),
-        'employees_quantity': fields.char("Employees Quantity", size="16"),
+        'employees_quantity': fields.char("Employees Quantity", size=16),
     }
